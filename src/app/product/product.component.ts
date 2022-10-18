2744c1982abb282c69ada8f6a5c98a6b3e6566ce
quyet <=>
2022-10-18 14:18:11 +0700

    done

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  color: string;
+  excute: string;
+  result: number;
   constructor() { }
 
   ngOnInit() {
   }
-
+  excutes(excute: string) {
+    this.excute = excute;
+  }
+  colors(color: string) {
+    this.color = color;
+  }
+  results(): number {
+    // tslint:disable-next-line:no-eval
+    this.result = eval(this.excute);
+    this.colors(this.color);
+    return this.result;
+  }
 }
